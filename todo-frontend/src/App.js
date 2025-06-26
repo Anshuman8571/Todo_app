dc7aecd24abdabb9c5ab34e23e5961c7e5c8f033
Anshuman8571 <anshumanbhardwaj2011@gmail.com>
2025-06-27 02:49:36 +0530

    Created Frontend for Todo App

---
+++ todo-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import AddTodo from './components/AddTodo';
+
+function App() {
+  return (
+    <div className="App">
+      <AddTodo>
+        
+      </AddTodo>
+        
+    </div>
+  );
+}
+
+export default App;
