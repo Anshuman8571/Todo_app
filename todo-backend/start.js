2f7d06f4da1c4d6cde8292d83fb4c858011c792b
Anshuman8571 <anshumanbhardwaj2011@gmail.com>
2025-08-27 16:00:04 +0530

    Added Dockerfile in .gitignore to check the backend anf frontend

--- todo-backend/start.js
+++ todo-backend/start.js
@@ -1,11 +1,7 @@
 const app = require("./server.js");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 console.log("The server will be starting");
 app.listen(PORT, () => { // After get(), this tells where it-is about to happen
     console.log(`Server is running on the port ${PORT}`)
 })
-
-
-
-
